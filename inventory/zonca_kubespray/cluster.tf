46bef7bb0853bb145cc2ef1e2709981cfb9c3041
Andrea Zonca <code@andreazonca.com>
2018-08-30 16:39:18 -0700

    cluster configuration

---
+++ inventory/zonca_kubespray/cluster.tf
@@ -0,0 +1,47 @@
+# your Kubernetes cluster name here
+cluster_name = "kubespray"
+
+# SSH key to use for access to nodes
+public_key_path = "~/.ssh/id_rsa.pub"
+
+# image to use for bastion, masters, standalone etcd instances, and nodes
+image = "Ubuntu 16.04 Devel and Docker v.1.19"
+# user on the node (ex. core on Container Linux, ubuntu on Ubuntu, etc.)
+ssh_user = "ubuntu"
+
+# 0|1 bastion nodes
+number_of_bastions = 0
+#flavor_bastion = "<UUID>"
+
+# standalone etcds
+number_of_etcd = 0
+
+# masters
+number_of_k8s_masters = 1
+number_of_k8s_masters_no_etcd = 0
+number_of_k8s_masters_no_floating_ip = 0
+number_of_k8s_masters_no_floating_ip_no_etcd = 0
+flavor_k8s_master = "3"
+
+# nodes
+number_of_k8s_nodes = 2
+number_of_k8s_nodes_no_floating_ip = 2
+flavor_k8s_node = "3"
+
+# GlusterFS
+# either 0 or more than one
+#number_of_gfs_nodes_no_floating_ip = 0
+#gfs_volume_size_in_gb = 150
+# Container Linux does not support GlusterFS
+#image_gfs = "<image name>"
+# May be different from other nodes
+#ssh_user_gfs = "ubuntu"
+#flavor_gfs_node = "<UUID>"
+
+# networking
+network_name = "zonca-k8s-kubespray-internal"
+external_net = "4367cd20-722f-4dc2-97e8-90d98c25f12e"
+#subnet_cidr = "<cidr>"
+floatingip_pool = "public"
+
+az_list = ["zone-r7"]
