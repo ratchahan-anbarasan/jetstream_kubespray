5e11278f5ffe8de21f8a72000363ff56c4475c51
Andrea Zonca <code@andreazonca.com>
2018-09-18 20:08:04 -0700

    open http and https ports bastion security group terraform

--- contrib/terraform/openstack/modules/compute/main.tf
+++ contrib/terraform/openstack/modules/compute/main.tf
@@ -25,6 +25,20 @@ resource "openstack_compute_secgroup_v2" "bastion" {
     to_port     = "22"
     cidr        = "0.0.0.0/0"
   }
+
+  rule {
+    ip_protocol = "tcp"
+    from_port   = "80"
+    to_port     = "80"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    ip_protocol = "tcp"
+    from_port   = "443"
+    to_port     = "443"
+    cidr        = "0.0.0.0/0"
+  }
 }
 
 resource "openstack_compute_secgroup_v2" "k8s" {
