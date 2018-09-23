000e9aa5369a93a0500351f35c6bb0b7231bdec6
Andrea Zonca <code@andreazonca.com>
2018-09-22 21:48:52 -0700

    replace underscores with dashes in cluster name

--- inventory/zonca_kubespray/terraform_apply.sh
+++ inventory/zonca_kubespray/terraform_apply.sh
@@ -1,1 +1,1 @@
-terraform apply -auto-approve -var-file=cluster.tf -var "cluster_name=$CLUSTER" ../../contrib/terraform/openstack
+terraform apply -auto-approve -var-file=cluster.tf -var "cluster_name=${CLUSTER/_/-}" ../../contrib/terraform/openstack
--- inventory/zonca_kubespray/terraform_init.sh
+++ inventory/zonca_kubespray/terraform_init.sh
@@ -1,1 +1,1 @@
-terraform init -var-file=cluster.tf -var "cluster_name=$CLUSTER" ../../contrib/terraform/openstack
+terraform init -var-file=cluster.tf -var "cluster_name=${CLUSTER/_/-}" ../../contrib/terraform/openstack
