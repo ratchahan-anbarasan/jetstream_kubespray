b534a771e4a0391cd5fa2ab42291378f679e249e
Andrea Zonca <code@andreazonca.com>
2019-03-04 17:24:20 -0800

    preserve floating ip

---
+++ inventory/zonca/terraform_destroy_keep_floatingip.sh
@@ -0,0 +1,23 @@
+# Terraform destroy does not allow to exclude a resource
+# we need to list all the resources we want to destroy
+# we need to also preserve the "router" resource which is
+# a dependency of the floating ip resource
+terraform destroy -var-file=cluster.tf -var "cluster_name=${CLUSTER/_/-}" \
+    -target=module.compute.openstack_compute_floatingip_associate_v2.k8s_master \
+    -target=module.compute.openstack_compute_instance_v2.k8s_master \
+    -target=module.compute.openstack_compute_instance_v2.k8s_node_no_floating_ip \
+    -target=module.compute.openstack_compute_keypair_v2.k8s \
+    -target=module.compute.openstack_networking_secgroup_rule_v2.bastion \
+    -target=module.compute.openstack_networking_secgroup_rule_v2.k8s \
+    -target=module.compute.openstack_networking_secgroup_rule_v2.k8s_master \
+    -target=module.compute.openstack_networking_secgroup_rule_v2.k8s_master_http \
+    -target=module.compute.openstack_networking_secgroup_rule_v2.k8s_master_https \
+    -target=module.compute.openstack_networking_secgroup_rule_v2.k8s_master_ping \
+    -target=module.compute.openstack_networking_secgroup_rule_v2.worker \
+    -target=module.compute.openstack_networking_secgroup_v2.bastion \
+    -target=module.compute.openstack_networking_secgroup_v2.k8s \
+    -target=module.compute.openstack_networking_secgroup_v2.k8s_master \
+    -target=module.compute.openstack_networking_secgroup_v2.worker \
+    -target=module.network.openstack_networking_network_v2.k8s \
+    -target=module.network.openstack_networking_subnet_v2.k8s \
+     ../../contrib/terraform/openstack
