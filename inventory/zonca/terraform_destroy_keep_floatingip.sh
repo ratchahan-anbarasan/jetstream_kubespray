e03e845668fe44bc1516f49e2771694f829f16bd
Andrea Zonca <code@andreazonca.com>
2019-03-04 17:28:50 -0800

    note on how to update

--- inventory/zonca/terraform_destroy_keep_floatingip.sh
+++ inventory/zonca/terraform_destroy_keep_floatingip.sh
@@ -2,6 +2,10 @@
 # we need to list all the resources we want to destroy
 # we need to also preserve the "router" resource which is
 # a dependency of the floating ip resource
+# this works for any number of master or nodes
+# you can update this by running:
+# `terraform plan -destroy -out=tf.plan` (also add other args from `terraform_destroy.sh` except -auto-approve
+# and then `terraform show tf.plan`
 terraform destroy -var-file=cluster.tf -var "cluster_name=${CLUSTER/_/-}" \
     -target=module.compute.openstack_compute_floatingip_associate_v2.k8s_master \
     -target=module.compute.openstack_compute_instance_v2.k8s_master \
