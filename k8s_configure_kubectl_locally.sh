ee5665c7ad29bd6890a9fcf94df840fdae033ea6
Andrea Zonca <code@andreazonca.com>
2022-01-09 09:58:04 -0800

    leave admin.conf in place

--- k8s_configure_kubectl_locally.sh
+++ k8s_configure_kubectl_locally.sh
@@ -1,1 +1,2 @@
-sed 's/10\.0\.0\.[[:digit:]]\+/'"$IP/g" inventory/$CLUSTER/artifacts/admin.conf > $HOME/.kube/config
+#sed 's/10\.0\.0\.[[:digit:]]\+/'"$IP/g" inventory/$CLUSTER/artifacts/admin.conf > $HOME/.kube/config
+sed -i 's/10\.0\.0\.[[:digit:]]\+/'"$IP/g" inventory/$CLUSTER/artifacts/admin.conf #> $HOME/.kube/config
